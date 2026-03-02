ed8ad1f2c952fdde41151878f7d116db15cb1edc
Maurice Wangleng Tan <tanwangleng@outlook.com>
2026-03-02 22:03:09 +0800

    chore: add template empty.zig

--- README.md
+++ README.md
@@ -1,1 +1,15 @@
 # advent-of-code-2025-zig
+
+To run a particular day:
+
+```
+zig run day01.zig
+```
+
+To test a particular day:
+
+```
+zig test day01.zig
+```
+
+TODO: Add a way to test all days through `build.zig`.
---
+++ empty.zig
@@ -0,0 +1,36 @@
+const std = @import("std");
+
+const ACTUAL_INPUT = @embedFile("./actual_inputs/2025/01/input.txt");
+
+fn p1(input: [:0]const u8) i64 {
+    _ = input;
+    return 1;
+}
+
+fn p2(input: [:0]const u8) i64 {
+    _ = input;
+    return 2;
+}
+
+pub fn main() !void {
+    std.debug.print("{d}\n", .{p1(ACTUAL_INPUT)});
+    std.debug.print("{d}\n", .{p2(ACTUAL_INPUT)});
+}
+
+const SAMPLE_INPUT = "";
+
+test "p1 sample" {
+    try std.testing.expectEqual(1, p1(SAMPLE_INPUT));
+}
+
+test "p1 actual" {
+    try std.testing.expectEqual(1, p1(ACTUAL_INPUT));
+}
+
+test "p2 sample" {
+    try std.testing.expectEqual(2, p2(SAMPLE_INPUT));
+}
+
+test "p2 actual" {
+    try std.testing.expectEqual(2, p2(ACTUAL_INPUT));
+}
